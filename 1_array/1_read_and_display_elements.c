b6c7e7894d48e12b9d05e1466bdb456844f466a3
surendrajngp <surendraj454@gmail.com>
2021-06-17 18:10:49 +0530

    Array

---
+++ 1_array/1_read_and_display_elements.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+    printf("Enter Size of Array : ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read
+    printf("\nEnter %d elements :\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Display
+    printf("\nElements are : ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
