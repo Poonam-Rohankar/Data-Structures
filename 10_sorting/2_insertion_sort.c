f855568c760a8787cecbacb60ade60682f74fe36
surendrajngp <surendraj454@gmail.com>
2021-07-05 10:21:23 +0530

    Insertion Sort

---
+++ 10_sorting/2_insertion_sort.c
@@ -0,0 +1,43 @@
+//  ! Wap to implement insertion sort
+
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void insertion_sort(int A[], int n)
+{
+    int i, j, x;
+
+    for (i = 1; i < n; i++)
+    {
+        j = i - 1;
+        x = A[i];
+
+        while (j > -1 && A[j] > x) // comparing element at index i with with all the elements less than index i
+        {
+            A[j + 1] = A[j]; // right shift
+            j--;
+        }
+        A[j + 1] = x; // finally place ith elemnt at sorted place
+    }
+}
+
+int main()
+{
+    int A[] = {23, 12, 54, 34, 10, 2, 78, 16, 0, 90}, n = 10, i;
+
+    insertion_sort(A, n);
+
+    printf("\nAfter Insertion Sort : ");
+    for (i = 0; i < n; i++)
+    {
+        printf("\t%d", A[i]);
+    }
+
+    return 0;
+}
